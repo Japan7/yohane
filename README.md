d4b8c3b506132b515f423a6c89696b69d27d0a76
NextFire <git@yuru.moe>
2023-10-28 19:30:36 -0400

    text_processing: use karaoke mugen auto-split

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Takes a song and its lyrics as input, extracts the vocals, splits the syllabes and computes a forced alignment to generate a karaoke in an [Aegisub](https://aegisub.org) subtitles file (.ass).
 
+This script is optimized for Japanese songs.
+
 ## Getting Started
 
 **Requirements:**
@@ -22,9 +24,10 @@ For a ~4 min song, on a MacBook Pro 2018 (i5-8259U), CPU only:
 
 ## Sample
 
-[Aqours - PV - HAPPY PARTY TRAIN](https://hikari.butaishoujo.moe/v/d6730f16/Aqours%20-%20PV%20-%20HAPPY%20PARTY%20TRAIN%20%28extract%29.mp4) (hardsub extract)
+[Aqours - PV - HAPPY PARTY TRAIN](https://hikari.butaishoujo.moe/v/d6730f16/Aqours%20-%20PV%20-%20HAPPY%20PARTY%20TRAIN%20%28extract%29.mp4) (hardsub extract, rev. [6cfa494](https://github.com/Japan7/yohane/commit/6cfa4944069b576b9c8accc3e90c684db02f9947))
 
 ## References
 
 - [Forced alignment for multilingual data – Torchaudio documentation](https://pytorch.org/audio/stable/tutorials/forced_alignment_for_multilingual_data_tutorial.html)
 - [tsurumeso/vocal-remover](https://github.com/tsurumeso/vocal-remover)
+- [Karaoke Mugen's auto-split.lua](https://docs.karaokes.moe/aegisub/auto-split.lua)
--- yohane/text_processing.py
+++ yohane/text_processing.py
@@ -1,6 +1,7 @@
-import re
 from dataclasses import dataclass
 from functools import cached_property
+
+import regex as re
 
 
 @dataclass
@@ -45,7 +46,7 @@ class Word:
 
     @classmethod
     def from_value(cls, value: str):
-        syllables = split_romaji_word(value)
+        syllables = split_word(value)
         return cls(value, syllables)
 
 
@@ -58,93 +59,14 @@ def normalize_uroman(text: str):
     return text.strip()
 
 
-def split_romaji_word(word: str):
-    """
-    https://github.com/animefn/ksplitter/blob/51ea5abde8542703c8d89143304b69f11576b678/ksplitter.py#L44C1-L157C27
-    """
-    karaSplit_array = []
-    ln = len(word)
-    letter_index = 0
-    while letter_index < ln:
-        letter = word[letter_index]
-        letter1 = ""
-        if 0 <= letter_index + 1 < len(word):
-            letter1 = word[letter_index + 1]
+# https://docs.karaokes.moe/aegisub/auto-split.lua
+MUGEN_RE = re.compile(
+    r"(?i)(?:(?<=[^sc])(?=h))|(?:(?<=[^kstnhfmrwpbdgzcj])(?=y))|(?:(?<=[^t])(?=s))|(?:(?=[ktnfmrwpbdgzcj]))|(?:(?<=[aeiou]|[^[:alnum:]])(?=[aeiou]))"
+)
 
-        if letter.lower() in "rymnhk":
-            if letter1.lower() in "aeiouō":
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-            else:
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
 
-        elif letter.lower() == "w":
-            if letter1.lower() in "aoō":
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-            else:
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-        # if letter ==  "y":
-        elif letter.lower() == "t":
-            if letter1.lower() in "aeoō":
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-            elif letter1.lower() == "s":
-                if 0 <= letter_index + 2 < len(word):
-                    letter2 = word[letter_index + 2]
-                    karaSplit_array.append(letter + letter1 + letter2)
-                letter_index = letter_index + 3
-            else:
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-
-        elif letter.lower() == "c":
-            if letter1.lower() == "h":
-                if 0 <= letter_index + 2 < len(word):
-                    letter2 = word[letter_index + 2]
-                    karaSplit_array.append(letter + letter1 + letter2)
-                letter_index = letter_index + 3
-            else:
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-
-        elif letter.lower() == "s":
-            if letter1.lower() in "aueoō":
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-            elif letter1.lower() == "h":
-                if 0 <= letter_index + 2 < len(word):
-                    letter2 = word[letter_index + 2]
-                    karaSplit_array.append(letter + letter1 + letter2)
-                letter_index = letter_index + 3
-            else:
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-        elif letter.lower() == "f":
-            if letter1 == "u":
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-            else:
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-
-        elif letter.lower() in "aeiou":
-            if letter1.lower() in "aeiou":
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-            else:
-                # this should not happen it is only the case above, nothing is supposed to end with these letter
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-
-        else:
-            if letter1.lower() in "aeiou":
-                karaSplit_array.append(letter + letter1)
-                letter_index = letter_index + 2
-            else:
-                karaSplit_array.append(letter)
-                letter_index = letter_index + 1
-
-    return karaSplit_array
+def split_word(word: str):
+    splitter_str, _ = MUGEN_RE.subn("#@", word)
+    syllabes = re.split("#@", splitter_str, flags=re.MULTILINE)
+    syllabes = list(filter(None, syllabes))
+    return syllabes
