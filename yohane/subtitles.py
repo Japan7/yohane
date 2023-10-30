3c5b095df7faa03cb77ea007a70af26bfe2b9d28
NextFire <git@yuru.moe>
2023-10-30 10:55:24 -0400

    subs: split syllables timing from ass generation

--- yohane/subtitles.py
+++ yohane/subtitles.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from functools import partial
 from importlib.metadata import metadata
 
@@ -13,7 +14,67 @@ PKG_META = metadata(__package__)
 IDENTIFIER = f"{PKG_META['Name']} {PKG_META['Version']} ({PKG_META['Home-page']})"
 
 
+@dataclass
+class TimedSyllable:
+    value: str
+    start_s: float  # s
+    end_s: float  # s
+
+    def k_duration(self, snap_to: float | None = None):
+        k_s = (snap_to if snap_to is not None else self.end_s) - self.start_s  # s
+        return round(k_s * 100)  # cs
+
+
 def make_ass(
+    lyrics: Lyrics,
+    waveform: Tensor,
+    emission: Tensor,
+    token_spans: list[list[TokenSpan]],
+):
+    all_line_syllables = time_lyrics(lyrics, waveform, emission, token_spans)
+
+    subs = SSAFile()
+    subs.info["Original Timing"] = IDENTIFIER
+
+    for line, syllables in zip(lyrics.lines, all_line_syllables):
+        start_syl = syllables[0]
+        end_syl = syllables[-1]
+        assert start_syl is not None and end_syl is not None
+
+        event = SSAEvent(round(start_syl.start_s * 1000), round(end_syl.end_s * 1000))
+
+        for i, syllable in enumerate(syllables):
+            if syllable is None:  # space
+                continue
+
+            value = syllable.value
+
+            snap_to_i = None
+            if i < len(syllables) - 1:  # not last syllable in line
+                if syllables[i + 1] is None:  # next is space
+                    value += " "
+                    snap_to_i = i + 2  # snap to syllable after the space
+                else:
+                    snap_to_i = i + 1  # snap to next syllable
+
+            if snap_to_i is not None:
+                snap_to_syl = syllables[snap_to_i]
+                assert snap_to_syl is not None
+                snap_to = snap_to_syl.start_s
+            else:
+                snap_to = None
+
+            k_duration = syllable.k_duration(snap_to=snap_to)  # cs
+            event.text += rf"{{\k{k_duration}}}{value}"
+
+        # save the raw line in a comment
+        comment = SSAEvent(event.start, event.end, line.raw, type="Comment")
+        subs.extend((comment, event))
+
+    return subs
+
+
+def time_lyrics(
     lyrics: Lyrics,
     waveform: Tensor,
     emission: Tensor,
@@ -25,45 +86,45 @@ def make_ass(
     sample_rate = bundle.sample_rate
     tokenizer = bundle.get_tokenizer()
 
-    subs = SSAFile()
-    subs.info["Original Timing"] = IDENTIFIER
-
     token_spans_iter = iter(token_spans)
-    add_syllable = partial(_add_syllable, ratio, sample_rate, tokenizer)
+    add_syllable = partial(_time_syllable, ratio, sample_rate, tokenizer)
+
+    all_line_syllables: list[list[TimedSyllable | None]] = []
 
     for line in lyrics.lines:
-        event: SSAEvent | None = None
-        k_cumul = 0
+        line_syllables: list[TimedSyllable | None] = []
 
         for word in line.words:
             spans = next(token_spans_iter)
             span_idx = 0
-            add_word_syllable = partial(add_syllable, spans)
+            time_syllable = partial(add_syllable, spans)
 
             for syllable in word.syllables:
-                event, nb_tokens, added_k = add_word_syllable(
-                    event, syllable, span_idx, k_cumul
-                )
+                nb_tokens, timed_syllable = time_syllable(syllable, span_idx)
                 span_idx += nb_tokens
-                k_cumul += added_k
+                line_syllables.append(timed_syllable)
 
-        if event is not None:
-            # save the raw line in a comment
-            comment = SSAEvent(event.start, event.end, line.raw, type="Comment")
-            subs.extend((comment, event))
+            line_syllables.append(None)
 
-    return subs
+        line_syllables = line_syllables[:-1]  # remove trailing None
+        all_line_syllables.append(line_syllables)
+
+    try:
+        next(token_spans_iter)  # make sure we used all spans
+        raise RuntimeError("not all spans were used")
+    except StopIteration:
+        pass
+
+    return all_line_syllables
 
 
-def _add_syllable(
+def _time_syllable(
     ratio: float,
     sample_rate: float,
     tokenizer: Wav2Vec2FABundle.Tokenizer,
     spans: list[TokenSpan],
-    event: SSAEvent | None,
     syllable: str,
     span_idx: int,
-    k_cumul: int,
 ):
     syllable_tokens = tokenizer([syllable])
     nb_tokens = len(syllable_tokens[0])
@@ -74,34 +135,6 @@ def _add_syllable(
     t_start = x0 / sample_rate  # s
     t_end = x1 / sample_rate  # s
 
-    added_k = 0  # cs
+    timed_syllable = TimedSyllable(syllable, t_start, t_end)
 
-    if event is None:
-        # new line, new event
-        event = SSAEvent(int(t_start * 1000))  # ms
-    elif span_idx == 0:
-        # k tag logic:
-        # new word starting on the same line
-        # add a space and adjust timing
-        space_dt = round(t_start * 100 - event.start / 10 - k_cumul)  # cs
-        event.text += rf"{{\k{space_dt}}} "
-        added_k += space_dt  # cs
-
-    # k tag logic:
-    # snap the token end time to the next token start time
-    try:
-        next_x0 = ratio * spans[span_idx + nb_tokens].start
-        next_t_start = next_x0 / sample_rate  # s
-        adjusted_t_end = next_t_start  # s
-    except IndexError:
-        adjusted_t_end = t_end  # s
-
-    k_duration = round((adjusted_t_end - t_start) * 100)  # cs
-    added_k += k_duration  # cs
-
-    event.text += rf"{{\k{k_duration}}}{syllable}"
-
-    # increment event end time
-    event.end = int(adjusted_t_end * 1000)  # ms
-
-    return event, nb_tokens, added_k
+    return nb_tokens, timed_syllable
