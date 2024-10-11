4da30bad54d68a9c3400f5cb119ca3bacfa854a1
NextFire <git@yuru.moe>
2024-10-11 23:16:16 +0200

    utils: fix get_identifier project link

--- yohane/utils.py
+++ yohane/utils.py
@@ -5,4 +5,4 @@ def get_identifier():
     if __package__ is None:
         raise ImportError("This module must be imported as a package")
     pkg_meta = metadata(__package__)
-    return f"{pkg_meta['Name']} {pkg_meta['Version']} ({pkg_meta['Home-page']})"
+    return f"{pkg_meta['Name']} {pkg_meta['Version']} ({pkg_meta['Project-URL']})"
